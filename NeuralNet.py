3cdb41d8b120c3057515028744450fd2fcf33296
xiaomingxu1995 <xiaoming.xu@mail.sdu.edu.cn>
2024-09-02 22:14:24 +0800

    update the requirement

--- NeuralNet.py
+++ NeuralNet.py
@@ -2,7 +2,6 @@ import torch
 import torch.nn as nn
 import torchvision
 import torchvision.transforms as transforms
-import matplotlib.pyplot as plt
 
 
 # Fully connected neural network with one hidden layer
--- train.py
+++ train.py
@@ -5,7 +5,6 @@ import torch
 import torch.nn as nn
 import torchvision
 import torchvision.transforms as transforms
-import matplotlib.pyplot as plt
 from NeuralNet import NeuralNet
 
 
